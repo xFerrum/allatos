9777fa493e96ee85c65d3eb9a3859ba8815dd10d
Márk Mészáros <mark.meszaros@inclouded.hu>
2024-02-04 12:35:58 +0100

    user registration feature barebones
    registration page is now a form, sign up button now creates firebase user and adds to collection "users"

--- src/app/pages/registration/registration.page.ts
+++ src/app/pages/registration/registration.page.ts
@@ -1,20 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { UserService } from 'src/services/user.service';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.page.html',
   styleUrls: ['./registration.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, ReactiveFormsModule],
+  providers: [UserService]
 })
-export class RegistrationPage implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+export class RegistrationPage implements OnInit
+{ 
+  form!: FormGroup;
+  constructor(public formBuilder: FormBuilder, public userService: UserService) {}
+  ngOnInit()
+  {
+    this.form = this.formBuilder.group({
+      username: ['', [Validators.required, Validators.minLength(4)]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$'),
+        ],
+      ],
+    });
   }
 
+  trySignUp()
+  {
+    if (this.form.valid) {
+      console.log(this.form.value);
+      this.userService.registerUser(this.form.get('username')!.value, this.form.get('email')!.value, this.form.get('password')!.value);
+    } else {
+      return console.log('Please provide all the required values!');
+    }
+  };
 }
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,21 +1,39 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { getFirestore, collection, getDocs, doc, addDoc } from 'firebase/firestore/lite';
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 const firebaseConfig =
 {
-    apiKey: "AIzaSyDznj0v_MEedP9vHiBHO7b468vlgaTN_48",
-    authDomain: "allatos.firebaseapp.com",
-    projectId: "allatos",
-    storageBucket: "allatos.appspot.com",
-    messagingSenderId: "766649715660",
-    appId: "1:766649715660:web:807d199ef4f3ffe1c36b69",
-    measurementId: "G-QHW9VJRNGC"
-  };
+  apiKey: "AIzaSyDznj0v_MEedP9vHiBHO7b468vlgaTN_48",
+  authDomain: "allatos.firebaseapp.com",
+  projectId: "allatos",
+  storageBucket: "allatos.appspot.com",
+  messagingSenderId: "766649715660",
+  appId: "1:766649715660:web:807d199ef4f3ffe1c36b69",
+  measurementId: "G-QHW9VJRNGC"
+};
   
   const fbase = initializeApp(firebaseConfig);
   const db = getFirestore(fbase);
+  const auth = getAuth();
 
 export class UserService
 {
-    getFbaseConfig() { return firebaseConfig };
+  registerUser(username: string, email: string, password: string)
+  {
+    createUserWithEmailAndPassword(auth, email, password)
+    .then(async () =>
+    {
+      await addDoc(collection(db, "users"),
+      {
+        email: email,
+        password: password,
+        username: username
+      });
+    })
+  .catch((error) =>
+  {
+    console.error(error.code + ": " + error.message);
+  });
+  }
 }
