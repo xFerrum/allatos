0ba16c605bdfe56123221d942aa22792c7f330db
xFerrum <xferrum0@gmail.com>
2024-06-23 12:25:58 +0200

    wss

--- src/app/tabs/creaturestab/creaturestab.page.ts
+++ src/app/tabs/creaturestab/creaturestab.page.ts
@@ -58,7 +58,7 @@ export class CreaturesPage implements OnInit, ViewWillLeave
 
   async learn(cr: Creature)
   {
-    this.socket = io('https://allatos-services.onrender.com:3005');
+    this.socket = io('wss://allatos-services.onrender.com:3005');
     //validate cr belongs to user (firebase rule)
     this.socket.emit('skill-learn-requested', this.userService.getLoggedInID(), cr.crID);
 
@@ -119,7 +119,7 @@ export class CreaturesPage implements OnInit, ViewWillLeave
 
   generateCreature()
   {
-    this.socket = io('https://allatos-services.onrender.com:3005');
+    this.socket = io('wss://allatos-services.onrender.com:3005');
     this.socket.emit('create-creature', this.userService.getLoggedInID());
     this.popUpService.loadingPopUp("Creating...");
     this.socket.on('creature-created', async () =>
@@ -154,7 +154,7 @@ export class CreaturesPage implements OnInit, ViewWillLeave
 
   fireAct(cr: Creature, act: Activity)
   {
-    this.socket = io('https://allatos-services.onrender.com:3010');
+    this.socket = io('wss://allatos-services.onrender.com:3010');
     act.startDate = new Date;
     this.socket.emit('start-activity', cr.crID, act);
     this.socket.on('start-activity-failed', () => { /*failed*/ });
@@ -162,7 +162,7 @@ export class CreaturesPage implements OnInit, ViewWillLeave
 
   attrPlus(cr: Creature, which: string)
   {
-    this.socket = io('https://allatos-services.onrender.com:3005');
+    this.socket = io('wss://allatos-services.onrender.com:3005');
     this.socket.emit('attr-plus', this.userService.getLoggedInID(), cr.crID, which);
 
     //calculated locally instantly so its more fluid
