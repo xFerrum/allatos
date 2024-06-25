75cfba50ccdb57398aed1fa56f775dffa4c5483d
Márk Mészáros <xferrum0@gmail.com>
2024-06-25 16:16:00 +0200

    min username: 3 letters

--- src/app/pages/registration/registration.page.ts
+++ src/app/pages/registration/registration.page.ts
@@ -23,7 +23,7 @@ export class RegistrationPage implements OnInit
   ngOnInit()
   {
     this.form = this.formBuilder.group({
-      username: ['', [Validators.required, Validators.minLength(4)]],
+      username: ['', [Validators.required, Validators.minLength(3)]],
       password: ['', [Validators.required, Validators.minLength(6)]],
       email: [
         '',
